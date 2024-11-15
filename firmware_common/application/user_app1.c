54ae345c22bc7afe12f8d21a55e0a41539ac2785
Harold Kim <harold.kim@ucalgary.ca>
2024-11-15 15:34:43 -0700

    midedit of Led basic vsc

--- firmware_common/application/user_app1.c
+++ firmware_common/application/user_app1.c
@@ -154,62 +154,7 @@ State Machine Function Definitions
 /* What does this state do? */
 static void UserApp1SM_Idle(void)
 {
-  static u16 u16BlinkCount = 0;
-  static u8 u8Counter = 0;
-  static u8 u8ColorIndex = 0;
-
-  static u8 aau8Color[][3] = {{RED0, 0xff, 0xff}, {RED0, GREEN0, 0xff}, 
-                              {0xff, GREEN0, 0xff}, {0xff, GREEN0, BLUE0},
-                              {0xff, 0xff, BLUE0}, {RED0, 0xff, BLUE0},
-                              {RED0, GREEN0, BLUE0}};
-  
-  u8 u8Offset = 0;
-  
-  for (u8 i = 0; i < (U8_TOTAL_LEDS - 1); i++) {
-    LedOff((LedNameType)i);
-  }
-
-  u16BlinkCount++;
-  if(u16BlinkCount == 250) {
-    u16BlinkCount = 0;
-    
-    u8Counter++;
-    if (u8Counter == 16) {
-      u8Counter = 0;
-    }
-  }
-
-  if(u8Counter & 0x01) {
-    u8Offset = 3;
-    for (u8 j = 0; j < 3; j++) {
-      if(aau8Color[u8ColorIndex][j] != 0xff)
-        LedOn((aau8Color[u8ColorIndex][j]) + u8Offset);
-    }
-  }
-
-  if(u8Counter & 0x02) {
-    u8Offset = ;
-    for (u8 j = 0; j < 3; j++) {
-      if(aau8Color[u8ColorIndex][j] != 0xff)
-        LedOn((aau8Color[u8ColorIndex][j]) + u8Offset);
-    }
-  }
-
-  if(u8Counter & 0x03) {
-    u8Offset = 9;
-    for (u8 j = 0; j < 3; j++) {
-      if(aau8Color[u8ColorIndex][j] != 0xff)
-        LedOn((aau8Color[u8ColorIndex][j]) + u8Offset);
-    }
-  }
-
-  if(u8Counter & 0x04) {
-    u8Offset = 12;
-    for (u8 j = 0; j < 3; j++) {
-      if(aau8Color[u8ColorIndex][j] != 0xff)
-        LedOn((aau8Color[u8ColorIndex][j]) + u8Offset);
-    }
-  }
+  HEARTBEAT_OFF();
   
 } /* end UserApp1SM_Idle() */
      
